7fa7f6494c160ce41427fb494d44cf909d8c49c1
payel <payel.nandy2580@gmail.com>
2022-07-07 06:29:43 -0400

    lab4

---
+++ fizz_buzz.py
@@ -0,0 +1,17 @@
+# for i in range(1,101):
+#     print(i)
+
+# for i in range(1,101):
+#     if i % 3 == 0:
+#         print("fizz")
+#         continue
+#         print(i)
+
+for i in range(1,101):
+    if i % 5 == 0:
+        print("buzz")
+        continue
+        print(i)
+
+
+
